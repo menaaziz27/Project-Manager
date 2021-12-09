fc5d578ec7c0362fef2b4254ab6b404b91ece349
menaaziz27 <menaaziz27@gmail.com>
2021-12-09 04:52:45 +0200

    update nested documents

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -5,9 +5,9 @@ const { Task } = require('../models/Task');
 
 exports.createTask = asyncHandler(async (req, res, next) => {
 	const { content } = req.body;
-	const { id } = req.params;
+	const { projectId } = req.params;
 
-	let project = await Project.findById(id);
+	let project = await Project.findById(projectId);
 	if (!project) {
 		res.status(401);
 		throw new Error('Cannot add a task to un existing project.');
@@ -19,4 +19,20 @@ exports.createTask = asyncHandler(async (req, res, next) => {
 	await task.save();
 
 	res.status(200).json({ task, project });
+});
+
+exports.updateTask = asyncHandler(async (req, res, next) => {
+	const { projectId, taskId } = req.params;
+	let project = await Project.findOneAndUpdate(
+		{ _id: projectId, 'tasks._id': taskId },
+		{
+			$set: { 'tasks[$].content': req.body.content, 'tasks[$].status': req.body.status },
+		}
+	);
+	if (!project) {
+		res.status(401);
+		throw new Error('Cannot add a task to un existing project.');
+	}
+
+	res.status(200).json({ project });
 });
--- backend/routes/task.js
+++ backend/routes/task.js
@@ -2,8 +2,11 @@ const express = require('express');
 const { Router } = express;
 
 const router = Router();
-const { createTask } = require('../controllers/taskController');
+const { createTask, updateTask } = require('../controllers/taskController');
 
-router.route('/api/tasks/:id').post(createTask);
+// router.route('/api/tasks/:id').post(createTask);
+// tasks
+router.route('/api/projects/:projectId/tasks').post(createTask);
+router.route('/api/projects/:projectId/tasks/:taskId').put(updateTask);
 
 module.exports = router;
